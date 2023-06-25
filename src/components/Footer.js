e17851a2e3246b2fc023a02d305be7e9d0fc57eb
hasekihurremsultan <136199686+hasekihurremsultan@users.noreply.github.com>
2023-06-26 00:37:58 +0300

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,17 @@
 
 import {motion} from "framer-motion";
 import {animation} from "~/Layout";
+import Link from "next/link";
+import {CgExternal} from "react-icons/cg";
 
-const Footer = () => <motion.footer {...animation} className="text-center pt-3 pb-6 text-gray-400 dark:text-gray-400 text-xs">
-    &copy; {new Date().getFullYear()} AppName - All rights reserved.
+const Footer = () => <motion.footer {...animation} className="text-center pt-3 pb-6 px-1 text-gray-400 dark:text-gray-400 text-xs flex justify-center gap-1">
+    &copy; {new Date().getFullYear()} AppName - All rights reserved. Project codes are hosted in
+    <Link
+        href="https://nextjs-simple-page-transition.vercel.app/"
+        className="underline hover:text-slate-800 transition-colors inline-flex items-center dark:hover:text-blue-200"
+    >
+        <CgExternal /> github
+    </Link>.
 </motion.footer>
 
 export default Footer;
