74d1bc58a94dbb53594c5da7dad819de7c4f1743
hasekihurremsultan <136199686+hasekihurremsultan@users.noreply.github.com>
2023-06-26 00:42:55 +0300

    github link added to footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,18 @@
 
 import {motion} from "framer-motion";
 import {animation} from "~/Layout";
-import Link from "next/link";
 import {CgExternal} from "react-icons/cg";
+
+const githubLink = process.env["GITHUB_LINK"] || "github.com";
 
 const Footer = () => <motion.footer {...animation} className="text-center pt-3 pb-6 px-1 text-gray-400 dark:text-gray-400 text-xs flex justify-center gap-1">
     &copy; {new Date().getFullYear()} AppName - All rights reserved. Project codes are hosted in
-    <Link
-        href="https://nextjs-simple-page-transition.vercel.app/"
+    <a
+        href={githubLink}
         className="underline hover:text-slate-800 transition-colors inline-flex items-center dark:hover:text-blue-200"
     >
         <CgExternal /> github
-    </Link>.
+    </a>.
 </motion.footer>
 
 export default Footer;
